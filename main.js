545207b7b7a1b31641b0367c194274a4b58b7108
JessicaGates <60752259+JessicaGates@users.noreply.github.com>
2020-05-02 12:50:25 -0500

    edits

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ let xItem = randomValueFromArray(insertX);
 let yItem = randomValueFromArray(insertY);
 let zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replace(':insertx',xItem);
-newStory = newStory.replace(':insertx',xItem);
-newStory = newStory.replace(':inserty',yItem);
-newStory = newStory.replace(':insertz',zItem);
+newStory = newStory.replace(':insertx:',xItem);
+newStory = newStory.replace(':insertx:',xItem);
+newStory = newStory.replace(':inserty:',yItem);
+newStory = newStory.replace(':insertz:',zItem);
 
 if(customName.value !==''){
   const name = customName.value;
